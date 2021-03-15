e34b506b6f247b5aa0d2e5aa6b878cffc747f5e3
jkachmar <git@jkachmar.com>
2021-03-14 20:51:58 -0400

    Reworks derivation entrypoint

--- default.nix
+++ default.nix
@@ -1,7 +1,5 @@
-{ fetchzip
-, installShellFiles
-, lib
-, lr
+{ callPackage
+, fetchzip
 , stdenv
   # Optional override for the HLS binaries to support specific GHC versions.
 , ghcVersions ? [
@@ -16,57 +14,50 @@
   ]
 }:
 let
-  inherit (stdenv) mkDerivation isDarwin;
+  inherit (stdenv) isDarwin isLinux;
   hlsBins = [ "wrapper" ] ++ ghcVersions;
-in
-mkDerivation rec {
+
+  #############################################################################
+  # Derivation attributes & metadata shared across platforms.
+  #############################################################################
+
   pname = "haskell-language-server";
   version = "1.0.0";
-  src =
-    if stdenv.isDarwin
-    then
-      fetchzip
-        {
-          url = "https://github.com/haskell/haskell-language-server/releases/download/${version}/haskell-language-server-macOS-${version}.tar.gz";
-          sha256 = "PXv8k7GebeHHsqOlgju2NIrubApg8JK8OpRNDevTqqU=";
-          stripRoot = false;
-        }
-    else
-      fetchzip
-        {
-          url = "https://github.com/haskell/haskell-language-server/releases/download/${version}/haskell-language-server-Linux-${version}.tar.gz";
-          sha256 = "oncBl94KOFjsg22mgxucAxa4T5Hq1SjmsGQ3yXXidjI=";
-          stripRoot = false;
-        };
+  meta = with stdenv.lib; {
+    description = ''
+      A language server that provides information about Haskell programs to
+      IDEs, editors, and other tools.
+    '';
+    homepage = "https://github.com/haskell/haskell-language-server";
+    license = licenses.asl20; # Apache-2.0 license.
+    maintainers = [ ];
 
-  nativeBuildInputs = [ installShellFiles ];
+    platforms = [ "x86_64-darwin" "x86_64-linux" ];
+  };
 
-  # NOTE: Copied from https://github.com/justinwoo/easy-dhall-nix/blob/master/build.nix
-  installPhase = ''
-      mkdir -p $out/bin
-    
-      ${lib.concatMapStringsSep "\n"
-        (hlsBin: ''
-          binPath="$out/bin/haskell-language-server-${hlsBin}"
-          # Install HLS
-          install -D -m555 -T "haskell-language-server-${hlsBin}" "$binPath"
-          rm "haskell-language-server-${hlsBin}"
-      
-          # Install bash completions.
-          "$binPath" --bash-completion-script "$binPath" > "haskell-language-server-${hlsBin}.bash"
-          installShellCompletion --bash "haskell-language-server-${hlsBin}.bash"
-          rm "haskell-language-server-${hlsBin}.bash"
-      
-          # Install zsh completions.
-          "$binPath" --zsh-completion-script "$binPath" > "haskell-language-server-${hlsBin}.zsh"
-          installShellCompletion --zsh "haskell-language-server-${hlsBin}.zsh"
-          rm "haskell-language-server-${hlsBin}.zsh"
-      
-          # Install fish completions.
-          "$binPath" --fish-completion-script "$binPath" > "haskell-language-server-${hlsBin}.fish"
-          installShellCompletion --fish "haskell-language-server-${hlsBin}.fish"
-          rm "haskell-language-server-${hlsBin}.fish"
-        '')
-    hlsBins}
-  '';
-}
+  #############################################################################
+  # Platform-Specific Derivations
+  #############################################################################
+
+  macosDrv = callPackage ./macos {
+    inherit hlsBins pname version meta;
+    src = fetchzip {
+      url = "https://github.com/haskell/haskell-language-server/releases/download/${version}/haskell-language-server-macOS-${version}.tar.gz";
+      sha256 = "PXv8k7GebeHHsqOlgju2NIrubApg8JK8OpRNDevTqqU=";
+      stripRoot = false;
+    };
+  };
+
+  nixosDrv = callPackage ./nixos {
+    inherit hlsBins pname version meta;
+    src = fetchzip {
+      url = "https://github.com/haskell/haskell-language-server/releases/download/${version}/haskell-language-server-Linux-${version}.tar.gz";
+      sha256 = "oncBl94KOFjsg22mgxucAxa4T5Hq1SjmsGQ3yXXidjI=";
+      stripRoot = false;
+    };
+  };
+in
+
+if isDarwin
+then macosDrv
+else nixosDrv
