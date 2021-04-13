a28c691b428f1b5c0b9625451ab63b71542d13be
jkachmar <git@jkachmar.com>
2021-04-13 16:03:52 -0400

    Nixifies test dependencies

--- test/shell.nix
+++ test/shell.nix
@@ -1,12 +1,15 @@
 { pkgs ? import <nixpkgs> { } }:
 let
   easy-hls = pkgs.callPackage ../default.nix { };
+  haskellDeps = pkgs.haskell.packages.ghc8104.ghcWithPackages
+    (haskellPkgs: with haskellPkgs; [ aeson ]);
 in
 
-pkgs.mkShell {
+pkgs.mkShell
+{
   buildInputs = with pkgs; [
     easy-hls
     cabal-install
-    haskell.compiler.ghc884
+    haskellDeps
   ];
 }
