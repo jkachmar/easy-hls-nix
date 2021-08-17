db85cac9d0405b4769b75cba0b004aed3beaf2de
jkachmar <git@jkachmar.com>
2021-08-16 20:36:05 -0400

    Updates shell.nix

--- shell.nix
+++ shell.nix
@@ -1,14 +1,7 @@
 { pkgs ? import <nixpkgs> { } }:
-let
-  easy-hls =
-      if pkgs.isDarwin
-      then (pkgs.callPackage ./default.nix { }).macosDrv
-      else (pkgs.callPackage ./default.nix { }).nixosDrv;
-
-in
 
 pkgs.mkShell {
   buildInputs = [
-    easy-hls
+    (pkgs.callPackage ./default.nix { })
   ];
 }
