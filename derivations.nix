c0fa0e71b6f2d9923d4b965ee29a48d80b859309
Gautier DI FOLCO <gautier.difolco@gmail.com>
2022-02-06 00:35:50 +0100

    Upgrade to HLS 1.6.1.0

--- derivations.nix
+++ derivations.nix
@@ -1,18 +1,16 @@
-{ callPackage
-, fetchzip
-, lib
-, stdenv
-  # Optional override for the HLS binaries to support specific GHC versions.
+{ callPackage, fetchzip, lib, stdenv
+# Optional override for the HLS binaries to support specific GHC versions.
 , ghcVersions ? [
-    "8.6.5"
-    "8.8.3"
-    "8.8.4"
-    "8.10.5"
-    "8.10.6"
-    "8.10.7"
-    "9.0.1"
-  ]
-}:
+  "8.6.5"
+  "8.8.3"
+  "8.8.4"
+  "8.10.5"
+  "8.10.6"
+  "8.10.7"
+  "9.0.1"
+  "9.0.2"
+  "9.2.1"
+] }:
 let
   inherit (stdenv) isDarwin isLinux;
   hlsBins = [ "wrapper" ] ++ ghcVersions;
@@ -22,7 +20,7 @@ let
   #############################################################################
 
   pname = "haskell-language-server";
-  version = "1.5.0";
+  version = "1.6.1.0";
   meta = {
     description = ''
       A language server that provides information about Haskell programs to
@@ -35,15 +33,14 @@ let
     platforms = [ "x86_64-darwin" "x86_64-linux" ];
   };
 
-
-in
-rec {
+in rec {
   #############################################################################
   # Platform-Specific Derivations
   #############################################################################
   nixosSrc = fetchzip {
-    url = "https://github.com/haskell/haskell-language-server/releases/download/${version}/haskell-language-server-Linux-${version}.tar.gz";
-    sha256 = "1cfqjdp43w3r9rs6w2x54kidll9j6q7521rymv81yaww6b1d4mg3";
+    url =
+      "https://github.com/haskell/haskell-language-server/releases/download/${version}/haskell-language-server-Linux-${version}.tar.gz";
+    sha256 = "05ldsqac227wddsdw3pf9jinmcafaw1rdq0f0kb5b0010p84m7wj";
     stripRoot = false;
   };
 
@@ -53,8 +50,9 @@ rec {
   };
 
   macosSrc = fetchzip {
-    url = "https://github.com/haskell/haskell-language-server/releases/download/${version}/haskell-language-server-macOS-${version}.tar.gz";
-    sha256 = "07731k2x10nqb8d2yzdbad4djmlmvhahrf3iw5kzr81y5xlw0ms5";
+    url =
+      "https://github.com/haskell/haskell-language-server/releases/download/${version}/haskell-language-server-macOS-${version}.tar.gz";
+    sha256 = "08zmy222z4gw7fansxnpv9v0yfv8lm9f9bklnz8dcm41hmy26hp6";
     stripRoot = false;
   };
 
